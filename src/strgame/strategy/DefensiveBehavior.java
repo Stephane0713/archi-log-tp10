0bbfc82829ee138f7f01a9bae82862cc0a610132
Mickael DP <m.dallepasqualine@gmail.com>
2023-11-20 15:16:38 +0100

    Added code StrategyPattern

--- src/strgame/strategy/DefensiveBehavior.java
+++ src/strgame/strategy/DefensiveBehavior.java
@@ -1,8 +1,9 @@
 package strgame.strategy;
 
 public class DefensiveBehavior implements CombatBehavior {
-    public void ..... {
+    public void execute() {
         // TODO: Implémentez le comportement défensif
         // Basez vous sur la classe de Test pour deviner ce que vous devez réaliser
+        System.out.println("Défense renforcée!");
     }
 }
--- src/strgame/strategy/FleeBehavior.java
+++ src/strgame/strategy/FleeBehavior.java
@@ -1,8 +1,9 @@
 package strgame.strategy;
 
 public class FleeBehavior implements CombatBehavior {
-    public void ...... {
+    public void execute() {
         // TODO: Implémentez le comportement de fuite
         // Basez vous sur la classe de Test pour deviner ce que vous devez réaliser
+        System.out.println("Fuite rapide!");
     }
 }
